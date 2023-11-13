6d4289e1e4b9eb28d8946135806d0e20a847fdc8
diyam <diyamed98@gmail.com>
2023-11-12 22:09:09 -0500

    url commit

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 # from django.conf.urls import include
-from django.urls import path
+from django.urls import path, include
 # Import router from rest_framework
 from rest_framework import routers
 from . import views
@@ -11,8 +11,9 @@ router.register('posts', views.PostViewSet)
 router.register('comments', views.CommentViewSet)
 
 urlpatterns = [
-  path('posts/', views.PostViewSet.as_view(), name='posts'),
-  path('comments/', views.CommentViewSet.as_view(), name='comments'),
+  # path('posts/', views.PostViewSet.as_view(), name='posts'),
+  # path('comments/', views.CommentViewSet.as_view(), name='comments'),
+  path('', include(router.urls)),
 
 ]
 
