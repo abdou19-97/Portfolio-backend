78995201aac1811723cb203ac8158cf9d2980538
diyam <diyamed98@gmail.com>
2023-12-05 12:02:09 -0500

    Dockerfile commit

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,8 @@ RUN pip install -r requirements.txt
 
 COPY . /app
 
+RUN python manage.py collectstatic --noinput --verbosity 3
+
 EXPOSE 8000
 
 CMD python manage.py runserver 0.0.0.0:8000
--- backend/settings.py
+++ backend/settings.py
@@ -153,6 +153,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR]  # Define your static directories here
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
